ac96bdda6d77936cf26a19de3522d97a8e57294d
Edgar Juan Guercio <jguercio@live.com.ar>
2024-02-13 19:44:03 -0300

    FEAT filter Publication type

--- Back/src/controllers/publication.controller.js
+++ Back/src/controllers/publication.controller.js
@@ -6,54 +6,60 @@ const cloudinary = require('cloudinary').v2;
 const service = new PublicationService();
 
 
-const create = async(req,res) =>{
+const create = async (req, res) => {
     try {
         // Subir la imagen a Cloudinary
         const result = await cloudinary.uploader.upload(req.file.path);
         // Obtener la URL de la imagen cargada desde Cloudinary
         const imageUrl = result.secure_url;
-        let {userId, petId, description, image_url, type} = req.body
-        
+        let { userId, petId, description, image_url, type } = req.body
+
         const response = await service.create({
-            userId, 
+            userId,
             petId,
-            description,  
+            description,
             image_url: imageUrl,
-            type, 
-            status:true
+            type,
+            status: true
         });
-        res.json({success: true, data: response});
+        res.json({ success: true, data: response });
     } catch (error) {
-        res.status(500).send({success:false,message:error.message});
+        res.status(500).send({ success: false, message: error.message });
     }
 }
 
-const update = async (req,res) =>{
+const update = async (req, res) => {
     try {
         // Subir la imagen a Cloudinary
         const result = await cloudinary.uploader.upload(req.file.path);
         // Obtener la URL de la imagen cargada desde Cloudinary
         const imageUrl = result.secure_url;
-        
-        let {description, publication_date, image_url, type,status} = req.body
-        const {id} = req.params;
 
-        const response= await service.update(id,{
-            description, 
-            publication_date, 
+        let { description, publication_date, image_url, type, status } = req.body
+        const { id } = req.params;
+
+        const response = await service.update(id, {
+            description,
+            publication_date,
             image_url: imageUrl,
-            type, 
+            type,
             status
         });
         res.json(response);
     } catch (error) {
-        res.status(500).send({success:false, message:error.message});
+        res.status(500).send({ success: false, message: error.message });
     }
 }
 
 
 const get = async (req, res) => {
-    await handleGet(req, res, service.find.bind(service));
+    try {
+        const { type } = req.query
+        const publications = await service.getPublications(type);
+        res.json(publications)
+    } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+    }
 };
 
 const getById = async (req, res) => {
@@ -77,6 +83,6 @@ const _deleted = async (req, res) => {
     await handleDeleted(req, res, service.deleted.bind(service), id, body);
 };
 
-module.exports ={
-    create,get,getById,update,_deleted, getByFkuserId, getByFkpetId
+module.exports = {
+    create, get, getById, update, _deleted, getByFkuserId, getByFkpetId
 };
