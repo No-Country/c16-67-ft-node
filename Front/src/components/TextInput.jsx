843cdbde8c3ecfe9ff2c5fd1e85ab89ffd22f6c3
unknown <lstradiot15@gmail.com>
2024-02-14 15:39:06 -0300

    Finished pets view

--- Front/src/components/TextInput.jsx
+++ Front/src/components/TextInput.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
-const TextInput = ({ labelName, inputType, placeholderText, error }) => {
+const TextInput = ({ labelName, input, placeholderText, error }) => {
   return (
     <div>
-      <label className="flex flex-col mx-auto my-auto text-center font-[700] text-[18px]">
+      <label className="flex flex-col mx-auto my-auto mb-3 text-center font-[700] text-[18px]">
         {labelName}
       </label>
-      <input
-        className="p-2 w-full mb-[10px] text-center border border-solid border-gray-300 rounded box-border shadow-md text-[18px] italic focus: outline-none "
-        placeholder={placeholderText}
-        type={inputType}
-      />
-      <span>{error}</span>
+      {input ? (
+        <input
+          className={`p-2 w-full mb-[10px] text-center border border-solid border-gray-300 rounded box-border shadow-md text-[14px] italic focus: outline-none ${error ? 'border-[#ea3354] focus:border focus:border-solid focus:border-[#ea3354]' : 'focus:border focus:border-solid focus:border-[#86a17c]'}`}
+          placeholder={placeholderText}
+        />
+      ) : (
+        <textarea
+          className={`p-2 w-full mb-[10px] text-center border border-solid border-gray-300 rounded box-border shadow-md text-[14px] italic focus: outline-none ${error ? 'border-[#ea3354] focus:border focus:border-solid focus:border-[#ea3354]' : 'focus:border focus:border-solid focus:border-[#86a17c]'}`}
+          placeholder={placeholderText}
+        ></textarea>
+      )}
+      <span className="block h-5 mb-1 text-[#ea3354] text-[14px]">{error}</span>
     </div>
   );
 };
