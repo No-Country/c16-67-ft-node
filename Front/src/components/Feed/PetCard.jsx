8202ff674f9d2239eb4fdf450d9148619c97ceb9
SchmidtAlejo <schmidtalejo@gmail.com>
2024-02-15 16:54:17 -0300

    Image and name of pet

--- Front/src/components/Feed/PetCard.jsx
+++ Front/src/components/Feed/PetCard.jsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+const API_URL_BASE = import.meta.env.VITE_SERVER_PRODUCTION;
+
 export default function PetCard({ postImage, description, petId, postId }) {
-  const profileImage =
-    'https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  const [pet, setPet] = useState({
+    name: '',
+    image_url: ''
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL_BASE}/api/v1/pet/${petId}`)
+      .then((res) => setPet({ name: res.data.name, image_url: res.data.image_url }));
+  }, []);
 
   return (
     <div className="mb-4">
       <div className="flex p-4 gap-x-3 items-center ">
-        <img className="w-12 h-12 object-cover rounded-full" src={profileImage} />
+        <img className="w-12 h-12 object-cover rounded-full" src={pet.image_url} />
         <div className="flex-grow">
-          <div>Tommy</div>
-          <div className="text-gray-500">@tommythehusky</div>
+          <div>{pet.name}</div>
+          <div className="text-gray-500">@{pet.name}</div>
         </div>
         <button className="text-white bg-gray-500 h-fit p-2 rounded-md">Follow</button>
       </div>
