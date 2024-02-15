ed74f9e32ce68ae4ec47adb5c613a097895f32e5
Edgar Juan Guercio <jguercio@live.com.ar>
2024-02-15 16:32:54 -0300

    EDIT maping data feed

--- Front/src/components/Feed/PetCard.jsx
+++ Front/src/components/Feed/PetCard.jsx
@@ -1,6 +1,6 @@
-export default function PetCard({postImage, description, petId, postId}) {
-  const profileImage =
-    'https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+export default function PetCard({postImage, description, petId, postId, petName, profileImage}) {
+  // const profileImage =
+  //   'https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
 
   return (
@@ -8,7 +8,7 @@ export default function PetCard({postImage, description, petId, postId}) {
       <div className="flex p-4 gap-x-3 items-center ">
         <img className="w-12 h-12 object-cover rounded-full" src={profileImage} />
         <div className="flex-grow">
-          <div>Tommy</div>
+          <div>{petName}</div>
           <div className="text-gray-500">@tommythehusky</div>
         </div>
         <button className="text-white bg-gray-500 h-fit p-2 rounded-md">Follow</button>
