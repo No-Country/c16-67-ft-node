9df3988e587e5ab9b96ec4a120259543b8c29897
Edgar Juan Guercio <jguercio@live.com.ar>
2024-02-16 18:12:47 -0300

    FEAT filter pets status

--- Front/src/pages/Profile.jsx
+++ Front/src/pages/Profile.jsx
@@ -2,34 +2,41 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 const API_URL_BASE = import.meta.env.VITE_SERVER_PRODUCTION;
+import Spinner from '../components/Spinner';
 
 export default function Profile() {
   const [options, setOptions] = useState([]);
   const [user, setUser] = useState({ name: '', image_url: '' });
   const userId = JSON.parse(localStorage.getItem('userId'));
   const pet = JSON.parse(localStorage.getItem('pet'));
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (userId === null) {
-      navigate('/login');
-    }
+    const fetchData = async () => {
+      setIsLoading(true);
+      if (userId === null) {
+        navigate('/login');
+        return;
+      }
 
-    axios.get(`${API_URL_BASE}/api/v1/pet/userid/${userId}`).then((res) => {
-      setOptions(
-        res.data.map((pet) => {
-          return {
-            value: pet.petId,
-            label: pet.name
-          };
-        })
-      );
-    });
-    axios.get(`${API_URL_BASE}/api/v1/user/${userId}`).then((res) => {
-      setUser(res.data);
-    });
-  }, []);
+      try {
+        const [petsResponse, userResponse] = await Promise.all([
+          axios.get(`${API_URL_BASE}/api/v1/pet/userid/${userId}`),
+          axios.get(`${API_URL_BASE}/api/v1/user/${userId}`),
+        ]);
+
+        setOptions(petsResponse.data.filter(pet => pet.status).map(pet => ({ value: pet.petId, label: pet.name })));
+        setUser(userResponse.data);
+      } catch (error) {
+        console.error("Error al obtener los datos:", error);
+        // TODO hacer modal o alerta error aca
+      }
+      setIsLoading(false)
+    };
+    fetchData();
+  }, [userId, navigate]);
 
   const onChange = (event) => {
     if (event.target.value === 'Agregar mascota') {
@@ -44,23 +51,30 @@ export default function Profile() {
 
   return (
     <div className="px-4">
+      {isLoading && <Spinner />}
       <div className="flex flex-col items-center gap-y-4 mt-4">
-        <img src={user.image_url} alt="User image" className="w-12 h-12 rounded-full" />
+        {!user.image_url?<></>:<img src={user.image_url} alt="User image" className="w-12 h-12 rounded-full" />
+        }  
         <p>{user.name}</p>
       </div>
-      <h2 className="mt-12">Mascotas</h2>
-      <select name="select" onChange={onChange} className="w-full">
-        <option>Agregar mascota</option>
-        {options.map((option) => (
-          <option
-            key={option.value}
-            value={option.value}
-            selected={pet !== null && option.value === pet.petId}
-          >
-            {option.label}
-          </option>
-        ))}
-      </select>
+      {(options.length===0)?<></>:
+      <section>
+        <h2 className="mt-12">Mascotas</h2>
+        <select name="select" onChange={onChange} className="w-full">
+          <option>Agregar mascota</option>
+          {options.map((option) => (
+            <option
+              key={option.value}
+              value={option.value}
+              selected={pet !== null && option.value === pet.petId}
+            >
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </section>
+      }
+
     </div>
   );
 }
