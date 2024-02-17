9f10e796b114b3091417e816b944f6921538f934
SchmidtAlejo <schmidtalejo@gmail.com>
2024-02-17 15:25:54 -0300

    css module

--- Front/src/components/HomeTab.jsx
+++ Front/src/components/HomeTab.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../public/assets/images/isologo.svg';
+import styles from './HomeTab.module.css';
 
 export default function HomeTab({ tabActive, setTabActive }) {
   return (
@@ -7,13 +8,13 @@ export default function HomeTab({ tabActive, setTabActive }) {
       <img src={logo} className="w-full hidden md:block xl:hidden h-12" alt="logo" />
       <div className="flex h-12 md:h-16 md:text-xl fixed md:static w-full bg-white">
         <div
-          className={`flex-grow basis-0 text-center h-full flex items-center justify-center cursor-pointer ${tabActive === 'Feed' ? 'border-secondary-300 border-b-2' : 'text-gray-600 border-gray-300 border-b-2'}`}
+          className={`${styles.tab} ${tabActive === 'Feed' ? `${styles.active}` : `${styles.inactive}`}`}
           onClick={() => setTabActive('Feed')}
         >
           Feed
         </div>
         <div
-          className={`flex-grow basis-0 text-center h-full flex items-center justify-center cursor-pointer ${tabActive === 'Lost-Adption' ? 'border-secondary-300 border-b-2' : 'text-gray-600 border-gray-300 border-b-2'}`}
+          className={`${styles.tab} ${tabActive === 'Lost-Adption' ? `${styles.active}` : `${styles.inactive}`}`}
           onClick={() => setTabActive('Lost-Adption')}
         >
           Lost/In adoption
