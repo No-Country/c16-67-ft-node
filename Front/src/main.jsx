c093930a2e1ed1eab5dafa084246d2bb21429979
unknown <lstradiot15@gmail.com>
2024-02-19 18:22:53 -0300

    Finished create-pet modal view

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -7,7 +7,6 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import PetsForm from './pages/PetsForm';
 import PublicationForm from './pages/PublicationForm';
 import Profile from './pages/Profile';
 import Menu from './pages/Menu';
@@ -23,7 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
-            <Route exact path="/pets-create" element={<PetsForm />} />
             <Route exact path="/publication-create" element={<PublicationForm />} />
             <Route exact path="/profile" element={<Profile />} />
             <Route exact path="/settings" element={<Menu />} />
