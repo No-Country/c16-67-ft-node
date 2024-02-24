b44f6cead5ed99cd8e6a351ce5815651d187196e
unknown <lstradiot15@gmail.com>
2024-02-24 14:40:56 -0300

    Finished profile view

--- Front/src/pages/Profile.jsx
+++ Front/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import { useModalContext } from '../context/modalContext';
 import Modal from '../components/Modal';
 import { useUserContext } from '../context/userContext';
 import { useNavigateContext } from '../context/navigationContext';
+import Suggestions from '../components/Suggestions';
 import { MdEdit } from 'react-icons/md';
 import Select from 'react-select';
 
@@ -127,26 +128,33 @@ export default function Profile() {
     }
   };
 
-  const customStyles = {
-    control: (provided, state) => ({
+  const customStylesSelect = {
+    control: (provided) => ({
       ...provided,
       width: '100%',
       height: '32px',
       borderRadius: '8px 8px 5px 5px',
-      border: state.isFocused ? 'none' : '2px solid #E9D0BD',
-      boxShadow: state.isFocused ? '0 0 0 1px #E9D0BB' : 'none',
+      border: '1px solid #E9D0BD',
+      boxShadow: 'none',
       outline: 'none',
       fontSize: '16px',
       '&:hover': {
-        border: '2px solid #cfad93'
+        backgroundColor: '#F3F4F6',
+        borderRadius: '8px 8px 5px 5px'
+      },
+      '@media (min-width: 768px)': {
+        border: 'none',
+        borderRadius: '0',
+        fontSize: '15px'
       }
     }),
     option: (provided, state) => ({
       ...provided,
       width: '100%',
-      backgroundColor: state.isSelected ? '#F8F0EA' : '#F8F0EA',
+      backgroundColor: state.isSelected ? '#c9b4a4' : '#F8F0EA',
       fontSize: '16px',
-      color: state.isSelected ? '#000' : '#000',
+      fontWeight: '600',
+      color: '#000',
       '&:hover': {
         backgroundColor: state.isSelected ? '#c39f84' : '#ddcbbd',
         color: '#FFFFFF'
@@ -155,87 +163,135 @@ export default function Profile() {
     menu: (provided) => ({
       ...provided,
       marginTop: '0',
-      padding: '0',
+      border: 'none',
       backgroundColor: '#F3F4F6',
       borderRadius: '0 0 8px 8px',
       boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)'
+    }),
+    menuList: (provided) => ({
+      ...provided,
+      paddingBottom: '0',
+      paddingTop: '2px',
+      backgroundColor: '#B8682A'
+    }),
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      display: 'none'
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      color: '#2D3748',
+      padding: '2px 8px'
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      padding: '0'
     })
   };
 
   return (
-    <main className="p-0">
-      {isLoading && <Spinner />}
-      {modalState.isOpen && <Modal />}
-      <div className="px-4">
-        <div className="flex flex-col items-center gap-y-4 mt-4">
-          <div className="flex justify-between items-center w-full text-[28px]">
-            {options.length === 0 ? (
-              <></>
-            ) : (
-              <section>
-                <section>
-                  <Select
-                    options={[...options, { value: 'addPet', label: 'Add pet' }]}
-                    onChange={(selectedOption) => onSelectPet(selectedOption)}
-                    value={options.find(
-                      (option) =>
-                        option.value === pet.petId ||
-                        options.find((option) => option.value === 'addPet')
-                    )}
-                    getOptionLabel={(option) =>
-                      option.value === 'addPet' ? option.label : `@${option.label}`
-                    }
-                    styles={customStyles}
-                  />
-                </section>
-              </section>
-            )}
-            {!user.image_url ? (
-              <></>
-            ) : (
-              <img
-                src={user.image_url}
-                alt="User image"
-                className="w-[35px] h-[35px] rounded-full"
-              />
-            )}
-          </div>
-        </div>
-        <section className="flex flex-col items-center justify-center mt-5">
-          <div className="absolute flex flex-row-reverse left-0 right-6 top-[60px] text-[24px]">
-            <MdEdit className="cursor-pointer" />
-          </div>
-          <img
-            src={pet.image_url}
-            alt="Pet-image"
-            className="w-[100px] h-[100px] mb-3 rounded-full shadow-lg object-cover"
-          />
-          <p className="text-[24px] mt-1 mb-5 text-[#232220]">{pet.name}</p>
-          <div className="flex justify-center w-[50%] text-[#176543] text-[16px] font-black">
-            <p className="text-center border-r-[1px] border-[#176543] mr-3 pr-3">1000 Followers</p>
-            <p className="text-center">500 Following</p>
-          </div>
-          <p className="mt-4 text-[16px]">{pet.description}</p>
-          <div className="flex flex-col items-center mt-8 w-full">
-            {publications.length === 0 ? (
-              <section className="flex justify-center h-[200px] p-4 shadow-lg bg-[#fafafa] font-semibold">
-                <div className="flex items-center">There´s no publication yet</div>
-              </section>
-            ) : (
-              <div className="grid grid-cols-2 gap-4 mb-[120px]">
-                {publications.map((publication, index) => (
+    <main className="pt-0">
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          {modalState.isOpen && <Modal />}
+          <div className="px-4">
+            <div className="flex flex-col items-center gap-y-4 mt-4">
+              <div className="flex justify-between items-center w-full text-[28px] md:hidden">
+                {options.length === 0 ? (
+                  <></>
+                ) : (
+                  <section>
+                    <Select
+                      options={[...options, { value: 'addPet', label: 'Add pet' }]}
+                      onChange={(selectedOption) => onSelectPet(selectedOption)}
+                      value={options.find(
+                        (option) =>
+                          option.value === pet.petId ||
+                          options.find((option) => option.value === 'addPet')
+                      )}
+                      getOptionLabel={(option) =>
+                        option.value === 'addPet' ? option.label : `@${option.label}`
+                      }
+                      styles={customStylesSelect}
+                    />
+                  </section>
+                )}
+                {!user.image_url ? (
+                  <></>
+                ) : (
                   <img
-                    key={index}
-                    src={publication.image_url}
-                    alt={`Publication-${index}`}
-                    className="w-[160px] h-[160px] object-cover rounded-md shadow-lg"
+                    src={user.image_url}
+                    alt="User image"
+                    className="w-[35px] h-[35px] rounded-full xl:hidden"
                   />
-                ))}
+                )}
               </div>
-            )}
+            </div>
+            <div className="xl:flex">
+              <section className="flex flex-col items-center justify-center mt-5 xl:w-[75%] w-full ">
+                <div className="absolute flex flex-row-reverse left-0 right-6 top-[60px] text-[24px] md:text-[35px] md:top-[20px] xl:right-[350px]">
+                  <MdEdit className="cursor-pointer" />
+                </div>
+                <img
+                  src={pet.image_url}
+                  alt="Pet-image"
+                  className="w-[100px] h-[100px] mb-3 rounded-full shadow-lg object-cover"
+                />
+                <p className=" mt-1 mb-2 text-[24px] text-[#232220] text-center md:mb-0 lg:text-[26px]">
+                  {pet.name}
+                </p>
+                {options.length === 0 ? (
+                  <></>
+                ) : (
+                  <section className="hidden md:block md:mb-4 md:mt-2">
+                    <Select
+                      options={[...options, { value: 'addPet', label: 'Add pet' }]}
+                      onChange={(selectedOption) => onSelectPet(selectedOption)}
+                      value={options.find(
+                        (option) =>
+                          option.value === pet.petId ||
+                          options.find((option) => option.value === 'addPet')
+                      )}
+                      getOptionLabel={(option) =>
+                        option.value === 'addPet' ? option.label : `@${option.label}`
+                      }
+                      styles={customStylesSelect}
+                    />
+                  </section>
+                )}
+                <div className="flex justify-center w-[200px] text-[#176543] font-black ">
+                  <p className="text-center text-[14px] md:text-[16px] border-r-[1px] border-[#176543] mr-3 pr-3">
+                    1000 Followers
+                  </p>
+                  <p className="text-center text-[14px] md:text-[16px]">500 Following</p>
+                </div>
+                <p className="mt-4 text-[16px]">{pet.description}</p>
+                <div className="flex flex-col items-center mt-8 w-full">
+                  {publications.length === 0 ? (
+                    <section className="flex justify-center h-[200px] p-4 shadow-lg bg-[#fafafa] font-semibold">
+                      <div className="flex items-center">There´s no publication yet</div>
+                    </section>
+                  ) : (
+                    <div className="grid grid-cols-2 gap-4 mb-[120px] lg:grid-cols-3">
+                      {publications.map((publication, index) => (
+                        <img
+                          key={index}
+                          src={publication.image_url}
+                          alt={`Publication-${index}`}
+                          className="w-[160px] h-[160px] object-cover rounded-md shadow-lg md:w-[204px] md:h-[224px] lg:w-[224px] lg:h-[244px]"
+                        />
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </section>
+              <Suggestions className="xl:ml-auto" />
+            </div>
           </div>
-        </section>
-      </div>
+        </>
+      )}
     </main>
   );
 }
