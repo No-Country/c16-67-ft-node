d07586da101e613b1246db5aac871016f096eddc
unknown <lstradiot15@gmail.com>
2024-02-25 18:49:43 -0300

    Starting with the new landing view

--- Front/src/pages/Login.jsx
+++ Front/src/pages/Login.jsx
@@ -1,30 +1,28 @@
 import { React } from 'react';
 import LoginContent from '../components/login/LoginContent';
 import logo from '../assets/images/isologo.svg';
-import foots from '../assets/images/foots.png';
+import catsLanding from '../assets/images/catsLanding.png';
 
 export default function Login() {
   return (
-    <main className="fixed bg-[#73360A] w-full h-full p-0 ">
-      <div className="fixed flex flex-col top-0 bottom-0 right-0 left-0 justify-center items-center m-2 rounded-[10px] bg-gradient-to-t from-white to-[#d9ae90]">
+    <>
+      <main className="fixed flex flex-col py-10 top-0 bottom-0 right-0 left-0 justify-start items-center bg-[#F8F0EA]">
+        <img src={logo} alt="isologo" className="w-[179px] h-[50px]" />
+        <div className="flex flex-col justify-end mt-[20px]">
+          <h3 className="mb-5 text-[#232220CC] text-[16px] font-semibold">By PetDevs</h3>
+        </div>
+        <img src={catsLanding} alt="Cats" className="w-[337px] h-[376px]" />
+        <p className="w-[273px] text-[15px]">
+          The social media for pets, where they are the main characters.
+        </p>
         <div className="flex flex-col h-[150px]">
-          <p className="font-walter  text-[30px] text-[#000] text-center">Welcome to</p>
-          <img src={logo} alt="isologo" />
+          <p className="hidden font-walter text-[30px] text-[#000] text-center md:block">
+            Welcome to
+          </p>
+          <img src={logo} alt="isologo" className="hidden" />
         </div>
-        <div className="flex items-center justify-center z-50">
-          <LoginContent />
-        </div>
-        <div className="flex flex-col justify-end h-[100px] mb-[50px]">
-          <h3 className="font-medium">By PetDevs</h3>
-        </div>
-      </div>
-      <div className="absolute flex justify-center w-full bottom-0 z-10">
-        <img
-          src={foots}
-          alt="foot-image"
-          className="md:max-w-[500px] mb-[-140px] animate-top opacity-30"
-        />
-      </div>
-    </main>
+        <LoginContent />
+      </main>
+    </>
   );
 }
