9171529dd71630d1a716277d41120a4108079340
unknown <lstradiot15@gmail.com>
2024-02-26 18:05:01 -0300

    Finished landing view

--- Front/src/pages/Login.jsx
+++ Front/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import LoginContent from '../components/login/LoginContent';
+import LoginContent from '../components/authentication/LoginContent';
 import logo from '../assets/images/isologo.svg';
 import catsLanding from '../assets/images/catsLanding.png';
 import walkFoots from '../assets/images/walkFoots.png';
@@ -31,19 +31,23 @@ export default function Login() {
             <img src={logo} alt="isologo" className="w-[179px] h-[50px]" />
             <img src={catsLanding} alt="Cats" className=" w-[510px] h-[520px]" />
           </div>
-          <div className="flex flex-col justify-evenly mr-20">
-            <span>
-              <p className="text-[45px] text-primary-700">Paws to everyone!</p>
-            </span>
-            <p className="font-walter text-[30px]">Welcome to</p>
-            <img src={logo} alt="isologo" className="w-[179px] h-[50px]" />
+          <div className="flex flex-col justify-around items-center mr-20 my-20 ">
+            <div className="flex flex-col justify-around items-center h-[35%]">
+              <span>
+                <p className="text-[45px] text-primary-700">Paws to everyone!</p>
+              </span>
+              <p className="font-walter text-[30px] mt-14">Welcome to</p>
+              <img src={logo} alt="isologo" className="w-[413px] h-[100px]" />
+            </div>
             <div className="flex flex-col mt-[20px]">
               <h3 className="mb-5 text-[#232220CC] text-[16px] font-semibold">By PetDevs</h3>
             </div>
-            <p className="w-[273px] text-[15px]">
-              The social media for pets, where they are the main characters.
-            </p>
-            <LoginContent />
+            <div className="flex flex-col justify-evenly h-[50%] items-center">
+              <p className="w-[560px] text-[22px] text-center">
+                The social media for pets, where they are the main characters.
+              </p>
+              <LoginContent />
+            </div>
           </div>
         </div>
       </main>
