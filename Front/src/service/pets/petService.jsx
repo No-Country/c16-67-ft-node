678b9866c8dd65b4a7d66c6ad92affab209bf06e
Edgar Juan Guercio <jguercio@live.com.ar>
2024-03-07 00:38:51 -0300

    JOIN services pet

--- Front/src/service/pets/petService.jsx
+++ Front/src/service/pets/petService.jsx
@@ -33,12 +33,58 @@ export const getPetsByName = async (name) => {
   }
 };
 
-export const createPet = async (payload) => {
-  return await axios.post(`${API_URL_BASE}/api/v1/pet`, payload, {
+export const createPet = async (formData) => {
+  return await axios.post(`${API_URL_BASE}/api/v1/pet`, formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
   });
+};
+
+export const setFormAndPostPet = ({
+  username,
+  age,
+  descriptions,
+  profilePhoto,
+  setIsLoading,
+  setActivePet,
+  changeLastPet,
+  openModal,
+  navigate
+}) => {
+  const fileInput = document.getElementById('fileInput');
+  const userId = JSON.parse(localStorage.getItem('userId'));
+  fileInput.files[0];
+
+  const formData = new FormData();
+  formData.append('name', name);
+  formData.append('username', username);
+  formData.append('age', age);
+  formData.append('description', descriptions);
+  formData.append('image', profilePhoto);
+  formData.append('userId', userId);
+
+  setIsLoading(true);
+
+  createPet(formData)
+    .then((response) => {
+      setActivePet(response.data.data);
+      changeLastPet(userId, response.data.data.petId).then(() => {
+        openModal({
+          description: 'Pet created successfully',
+          chooseModal: false
+        });
+      });
+      navigate('/');
+    })
+    .catch((error) => {
+      openModal({
+        description: `${error.response.data.message}`,
+        chooseModal: false,
+        error: true
+      });
+      setIsLoading(false);
+    });
 };
 
 export const editPet = async (petId, inputsData, file) => {
