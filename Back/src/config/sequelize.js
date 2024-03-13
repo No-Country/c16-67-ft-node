7c4bc9cb4242d083aed3dee37ad0c072229c002f
Edgar Juan Guercio <jguercio@live.com.ar>
2024-03-13 16:38:51 -0300

    CONFIG deploy db

--- Back/src/config/sequelize.js
+++ Back/src/config/sequelize.js
@@ -4,14 +4,15 @@ const setupModels = require('../database/index');
 
 
 const sequelize = new Sequelize(
-  config.dbName,
-  config.dbUser,
-  config.dbPassword,
-  {
-    host: config.dbHost,
-    dialect: 'postgres',
-    logging: false, // set to console.log to see the raw SQL queries,
-  }
+    config.dbName,
+    config.dbUser,
+    config.dbPassword,
+    {
+        host: config.dbHost,
+        port: config.dbPort,
+        dialect: 'postgres',
+        logging: false, // set to console.log to see the raw SQL queries,
+    }
 )
 
 setupModels(sequelize)
